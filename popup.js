ff5b0b202fbeb69ac8e99dfc304c655583539600
Abe Choi <abraham.choi@icloud.com>
2019-12-20 22:38:32 -0800

    Added undoColor function

--- popup.js
+++ popup.js
@@ -1,4 +1,5 @@
 let changeColor = document.getElementById('changeColor');
+let undoColor = document.getElementById('undoColor');
 
 chrome.storage.sync.get('color', function(data){
     changeColor.style.backgroundColor = data.color;
@@ -11,5 +12,13 @@ changeColor.onclick = function(element) {
       chrome.tabs.executeScript(
           tabs[0].id,
           {code: 'document.body.style.backgroundColor = "' + color + '";'});
+    });
+  };
+
+  undoColor.onclick = function(element) {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.executeScript(
+          tabs[0].id,
+          {code: 'document.body.style.backgroundColor = "#FFFFFF";'});
     });
   };
